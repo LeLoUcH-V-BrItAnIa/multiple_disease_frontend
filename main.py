0d8de5a3c438405f886097bd720a313a46efd9be
LeLoUcH-V-BrItAnIa <139791448+LeLoUcH-V-BrItAnIa@users.noreply.github.com>
2026-04-03 18:21:32 +0530

    UI chnage

--- main.py
+++ main.py
@@ -477,8 +477,8 @@ if st.session_state.page == "home":
         <p class="fade-text">
         Experience the future of healthcare with a smart AI system designed to analyze your health patterns and provide early insights.<br><br>
 
-        🧠 Detect potential risks like <b>Diabetes</b>, <b>Heart Conditions</b>, and <b>Neurological Disorders</b><br>
-        📊 Get personalized <b>lifestyle, diet, and wellness guidance</b><br>
+        🧠 Detect potential risks like <b>Diabetes</b>, <b>Heart Conditions</b>, and <b>Neurological Disorders</b>
+        📊 Get personalized <b>lifestyle, diet, and wellness guidance</b>
         ⚡ Powered by Machine Learning for faster and smarter predictions
         </p>
         """, unsafe_allow_html=True)
