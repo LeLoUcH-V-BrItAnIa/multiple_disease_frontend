32d6de486c62df69f2e9aec812f186d4c9a3f893
LeLoUcH-V-BrItAnIa <139791448+LeLoUcH-V-BrItAnIa@users.noreply.github.com>
2026-05-06 21:26:04 +0530

    Using doctor locator !

--- nearby_doctor.py
+++ nearby_doctor.py
@@ -41,7 +41,7 @@ def get_lat_long(place):
 # -------------------------------
 def fetch_doctors(lat, lon, radius=3000, specialist="All"):
     
-    url = "https://overpass.openstreetmap.fr/api/interpreter"
+    url = "https://overpass.openstreetmap.fr/api/interpreterr"
 
     query = f"""
     [out:json];
